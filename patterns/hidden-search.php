b6d53815ca3d81c9643bbd2a17c6c9fe91192c4c
Tammie <hello@tammielister.com>
2024-02-26 11:00:07 +0000

    Updates more templates and adds some basic query patterns

---
+++ patterns/hidden-search.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Search
+ * Slug: cashflow/hidden-search
+ * Inserter: no
+ */
+?>
+
+<!-- wp:search {"label":"<?php echo esc_attr_x( 'Search', 'search form label', 'cashflow' ); ?>","showLabel":false,"buttonText":"<?php echo esc_attr_x( 'Search', 'search button text', 'cashflow' ); ?>","fontSize":"medium"} /-->
--- patterns/template-home-membership.php
+++ patterns/template-home-membership.php
@@ -11,7 +11,7 @@
 
 <!-- wp:group {"tagName":"main","style":{"spacing":{"blockGap":"0","margin":{"top":"0"}}},"layout":{"type":"default"}} -->
 <main class="wp-block-group" style="margin-top:0">
-	<!-- wp:pattern {"slug":"twentytwentyfour/page-home-business"} /-->
+	<!-- wp:pattern {"slug":"cashflow/page-home-membership"} /-->
 </main>
 <!-- /wp:group -->
 
